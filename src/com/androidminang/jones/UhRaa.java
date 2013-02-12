105e83d603cb979fbfccd161e7df0d80770b06b0
dcsms <morondroid@gmail.com>
2013-02-12 10:06:26 -0800

    Init

---
+++ src/com/androidminang/jones/UhRaa.java
@@ -0,0 +1,16 @@
+package com.androidminang.jones;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UhRaa extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.uh_raa);
+	}
+
+	
+
+}
